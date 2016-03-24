1a34c8a296fce16a8b227b897bb27758720dd120
naelrouz <nael_rouz@mail.ru>
2016-03-24 12:23:49 +0300

    Начало

---
+++ lib/db.php
@@ -0,0 +1,18 @@
+<?php
+
+class ClassName {
+    
+//    $host = "";
+//    $user = "root";
+//    $password = "Nael2714";
+//    $database = "bh";
+
+    function __construct() {
+        $this->mysqli = new mysqli("localhost", "root", "Nael2714", "bh");
+    }
+    public function guery($sql) {
+        
+    }
+}
+
+
